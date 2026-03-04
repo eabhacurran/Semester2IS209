6fecee41f3d67c1a95ca53c4217b1b88ab4181d8
Administrator <eabhacurran8@gmail.com>
2026-03-04 09:48:49 +0000

    v1 done

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+API_KEY = os.getenv("API_KEY")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def index():
+    url = "https://api.thedogapi.com/v1/images/search"
+
+    headers = {
+        "x-api-key": API_KEY
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    image_url = data[0]["url"]
+
+    return render_template("index.html", image_url=image_url)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
